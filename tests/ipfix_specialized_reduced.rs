067a6e1eebac07c2b8c206ba96f317c4d9092d8e
Costa Tsaousis <costa@netdata.cloud>
2026-07-25 19:47:21 +0000

    fix: decode reduced forwarding status values

--- tests/ipfix_specialized_reduced.rs
+++ tests/ipfix_specialized_reduced.rs
@@ -17,22 +17,28 @@ fn message_with_set(id: u16, body: &[u8]) -> Vec<u8> {
 
 #[test]
 fn reduced_forwarding_status_is_decoded_as_an_unsigned_value() {
-    let mut template = Vec::new();
-    template.extend_from_slice(&256u16.to_be_bytes());
-    template.extend_from_slice(&1u16.to_be_bytes());
-    template.extend_from_slice(&89u16.to_be_bytes());
-    template.extend_from_slice(&2u16.to_be_bytes());
+    for (field_length, field_value) in [
+        (2u16, &[0, 1][..]),
+        (3, &[0, 0, 1][..]),
+        (4, &[0, 0, 0, 1][..]),
+    ] {
+        let mut template = Vec::new();
+        template.extend_from_slice(&256u16.to_be_bytes());
+        template.extend_from_slice(&1u16.to_be_bytes());
+        template.extend_from_slice(&89u16.to_be_bytes());
+        template.extend_from_slice(&field_length.to_be_bytes());
 
-    let mut parser = NetflowParser::default();
-    assert!(parser.parse_bytes(&message_with_set(2, &template)).is_ok());
+        let mut parser = NetflowParser::default();
+        assert!(parser.parse_bytes(&message_with_set(2, &template)).is_ok());
 
-    let decoded = parser.parse_bytes(&message_with_set(256, &[0, 1]));
-    assert!(decoded.error.is_none(), "{:#?}", decoded.error);
-    let NetflowPacket::IPFix(packet) = &decoded.packets[0] else {
-        panic!("expected IPFIX packet");
-    };
-    let FlowSetBody::Data(data) = &packet.flowsets[0].body else {
-        panic!("expected IPFIX data");
-    };
-    assert_eq!(data.fields[0][0].1.as_u64(), Some(1));
+        let decoded = parser.parse_bytes(&message_with_set(256, field_value));
+        assert!(decoded.error.is_none(), "{:#?}", decoded.error);
+        let NetflowPacket::IPFix(packet) = &decoded.packets[0] else {
+            panic!("expected IPFIX packet");
+        };
+        let FlowSetBody::Data(data) = &packet.flowsets[0].body else {
+            panic!("expected IPFIX data");
+        };
+        assert_eq!(data.fields[0][0].1.as_u64(), Some(1));
+    }
 }
